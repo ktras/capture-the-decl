0a6e14f7107056cf6c980e821ec1b2d561ee170c
Katherine Rasmussen <ktrasmussn@gmail.com>
2022-02-04 07:56:37 -0800

    Add ability for script to search for the module file in the common subdirectory, one level up from the current directory.

--- capture-the-decl.sh
+++ capture-the-decl.sh
@@ -17,13 +17,22 @@ fi
 sed -e 's/\use //g' -e 's/\, only : /.f90 "/g' -e 's/\, only: /.f90 "/g' -e 's/$/"/' $1 >> $tempdecls
 
 while read p; do
-    filename="$(echo $p | cut -d' ' -f 1)"
+    module_file="$(echo $p | cut -d' ' -f 1)"
+    if [[ ! -f $module_file ]]; then
+        if test -f ../common/$module_file; then
+            module_file="../common/$module_file"
+        else
+            echo "Module file not found in the common dir: $module_file"
+            echo "Please manually search for $module_file and its declarations in another directory"
+            continue
+        fi
+    fi
     vars_str="$(echo $p | cut -d'0' -f 2)"
     vars_str="$(echo $vars_str | cut -d'"' -f 2)"
     my_array=($(echo $vars_str | tr "," "\n"))
     for i in "${my_array[@]}"
     do
-        echo $(grep -w -i $i $filename) >> $finaldecls
+        echo $(grep -w -i $i $module_file) >> $finaldecls
     done
 done < $tempdecls
 
